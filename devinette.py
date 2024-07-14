336e136bd906467f9ced9dff2c510f751122063a
NKB0220 <nangabenjamin82@gmail.com>
2024-07-14 04:02:12 +0000

    Update devinette.py

--- devinette.py
+++ devinette.py
@@ -1,20 +1,3 @@
 import random
 z=random.randint(1,100)
-print("choisir un nombre entre 1 à 100")
-i=5
-# j'ai changé la valeur de i
-while i>=0:
-      print("essai n",i)
-      n=int(input(""))
-      print("proposition",n)
-      if n<z:
-         print("trop petit")
-      elif n>z:
-          print("trop grand")
-      else:
-          print("Bravo!! vous avez gagné")
-          break
-      i=i-1
-if n!=z:
-      print("desolé vous avez perdu")
 
